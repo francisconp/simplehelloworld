9a468fffe78e78c51a69ac46db72d44eda63acfc
Franciscon Pereira dos Santos <fpereira@Franciscons-MacBook-Pro.local>
2018-11-13 02:03:56 -0200

    Label, port and port

--- src/simplehelloworld.py
+++ src/simplehelloworld.py
@@ -10,6 +10,7 @@ from os import environ
 app = Flask(__name__)
 
 default_port = environ.get('default_port')
+default_label = environ.get('default_label')
 
 @app.route('/<path:path>')
 def static_file(path):
@@ -18,7 +19,10 @@ def static_file(path):
 @app.route("/")
 def root():
     var_hostname = gethostname()
-    return render_template('index.html', hostname=var_hostname)
+    msgs = { "hostname" : var_hostname,
+             "label": default_label,
+             "port": default_port }
+    return render_template('index.html', msgs=msgs)
 
 @app.route("/hostname")
 def hostname():
@@ -29,6 +33,12 @@ if default_port != None:
     pass
 else:
     print("Please set the default_port environment..")
+    exit(1)
+
+if default_label != None:
+    pass
+else:
+    print("Please set the default_label environment..")
     exit(1)
 
 if __name__ == "__main__":
